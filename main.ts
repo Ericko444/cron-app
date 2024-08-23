2c8cfd38f22e7c1920cf93a1904fd1c6b8c1687a
Ericko Andriamanarivo <ericko.andriamanarivo@gmail.com>
2024-08-23 12:22:32 +0300

    Cron config

---
+++ main.ts
@@ -0,0 +1,12 @@
+const Deno = require('deno');
+
+const APP_URLS = [
+    "https://practice-architecture.fly.dev",
+    "https://practice-archi-front.fly.dev"
+];
+
+Deno.cron("Ping URLs", "* * * * *", async () => {
+    for(const url of APP_URLS) {
+        await fetch(url);
+    }
+});
